74e08ea8ce488a44c6ca9327ecca66d36a607edb
Daniel Morales <danielmorales0000@yahoo.com>
2017-10-21 15:34:23 -0700

    Added a level selection activity and linked to the play button

--- app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/HomeActivity.java
+++ app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +13,11 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    public void onPlayButtonClicked(View v){
+        startActivity(new Intent(this, LevelSelectActivity.class));
+    }
+
     public void onHelpButtonClicked(View v){
         startActivity(new Intent(this, InstructionsActivity.class));
     }
