e5e9e9790f4a52ddaadccc3c51f4001aa87178cb
Reyna1237 <ortiz.reynac@gmail.com>
2017-10-21 14:43:12 -0700

    Remiving now ignored directory

--- app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/InstructionsActivity.java
+++ app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/InstructionsActivity.java
@@ -5,14 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class InstructionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_instructions);
     }
-    public void onHelpButtonClicked(View v){
-        startActivity(new Intent(this, InstructionsActivity.class));
-    }
+
 }
