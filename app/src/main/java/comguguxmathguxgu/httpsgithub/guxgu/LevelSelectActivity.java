7c837b11c96e96c64b777fd5f6b72c61b51c073b
Daniel Morales <danielmorales0000@yahoo.com>
2017-10-21 22:44:38 -0700

    Added sound effects to the buttons in the levelSelectActivity

--- app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/LevelSelectActivity.java
+++ app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/LevelSelectActivity.java
@@ -1,6 +1,8 @@
 package comguguxmathguxgu.httpsgithub.guxgu;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,128 +12,76 @@ import static android.R.attr.level;
 
 public class LevelSelectActivity extends AppCompatActivity {
 
+    private static SoundPool mSoundPool;
+    private static int buttonClinkSound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_select);
 
-        final Button level1 = (Button) findViewById(R.id.level1); //declaring the buttons
-        final Button level2 = (Button) findViewById(R.id.level2);
-        final Button level3 = (Button) findViewById(R.id.level3);
-        final Button level4 = (Button) findViewById(R.id.level4);
-        final Button level5 = (Button) findViewById(R.id.level5);
-        final Button level6 = (Button) findViewById(R.id.level6);
-        final Button level7 = (Button) findViewById(R.id.level7);
-        final Button level8 = (Button) findViewById(R.id.level8);
-        final Button level9 = (Button) findViewById(R.id.level9);
+        mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        buttonClinkSound = mSoundPool.load(getApplicationContext(), R.raw.button_click, 1);
 
-
-        level1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 1); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 2); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 3); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 4); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 6); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 6); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 7); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 8); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
-
-        level9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { //when the button is click
-
-                Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
-
-                intent.putExtra("num", 9); //sending the level number
-
-                startActivity(intent); // changing activities
-            }
-        });
     }
+
+    public void onLevel1Clicked(View v){
+        clickSound();
+        play(1);
+    }
+
+    public void onLevel2Clicked(View v){
+        clickSound();
+        play(2);
+    }
+
+    public void onLevel3Clicked(View v){
+        clickSound();
+        play(3);
+    }
+
+    public void onLevel4Clicked(View v){
+        clickSound();
+        play(4);
+    }
+
+    public void onLevel5Clicked(View v){
+        clickSound();
+        play(5);
+    }
+
+    public void onLevel6Clicked(View v){
+        clickSound();
+        play(6);
+    }
+
+    public void onLevel7Clicked(View v){
+        clickSound();
+        play(7);
+    }
+
+    public void onLevel8Clicked(View v){
+        clickSound();
+        play(8);
+    }
+
+    public void onLevel9Clicked(View v){
+        clickSound();
+        play(9);
+    }
+
+    public void clickSound()
+    {
+        mSoundPool.play(buttonClinkSound, 1.0f, 1.0f, 0, 0, 1.0f);
+    }
+
+    public void play(int levelNum)
+    {
+        Intent intent = new Intent(getApplicationContext(),PlayLevelActivity.class);
+        intent.putExtra("num", levelNum);
+        startActivity(intent);
+    }
+
+
+
 }
