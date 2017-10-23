62780228da2a5a4cbc895cc8a6e7e0597c596227
Daniel Morales <danielmorales0000@yahoo.com>
2017-10-22 17:07:22 -0700

    Added sound effect to the play button in homeActivity

--- app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/HomeActivity.java
+++ app/src/main/java/comguguxmathguxgu/httpsgithub/guxgu/HomeActivity.java
@@ -1,6 +1,8 @@
 package comguguxmathguxgu.httpsgithub.guxgu;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +10,19 @@ import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static SoundPool mSoundPool;
+    private static int buttonClinkSound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        buttonClinkSound = mSoundPool.load(getApplicationContext(), R.raw.button_click, 1);
     }
 
     public void onPlayButtonClicked(View v){
+        mSoundPool.play(buttonClinkSound, 1.0f, 1.0f, 0, 0, 1.0f);
         startActivity(new Intent(this, LevelSelectActivity.class));
     }
 
